06e5338db43a97a97d92cf440a609cae3db91d05
Pratik <pratik822@gmail.com>
2018-03-12 06:34:36 +0530

    ui changes

--- app/src/main/java/firebase/com/labs/ramdani/mygroupchat/LoginActivity.java
+++ app/src/main/java/firebase/com/labs/ramdani/mygroupchat/LoginActivity.java
@@ -1,16 +1,15 @@
 package firebase.com.labs.ramdani.mygroupchat;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,7 +19,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private ProgressDialog mProgressDialog;
+    private ProgressBar progressBar;
     private EditText edtEmail, edtPassword;
     private Button btnLogin;
 
@@ -31,59 +30,60 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setupUI();
+        mFirebaseAuth = FirebaseAuth.getInstance();
 
-        mProgressDialog = new ProgressDialog(this);
-        mProgressDialog.setTitle("Login");
-        mProgressDialog.setMessage("Please wait..");
+
+    }
+
+    public void setupUI() {
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
 
         edtEmail = (EditText) findViewById(R.id.edt_email);
         edtPassword = (EditText) findViewById(R.id.edt_password);
         btnLogin = (Button) findViewById(R.id.btn_login);
-
-        mFirebaseAuth = FirebaseAuth.getInstance();
         btnLogin.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.btn_login){
+        if (v.getId() == R.id.btn_login) {
 
             boolean isEmptyField = false;
 
-            final String email  = edtEmail.getText().toString().trim();
-            String password  = edtPassword.getText().toString().trim();
+            final String email = edtEmail.getText().toString().trim();
+            String password = edtPassword.getText().toString().trim();
 
             //validasi
 
-            if (TextUtils.isEmpty(email)){
+            if (TextUtils.isEmpty(email)) {
                 isEmptyField = true;
-                edtEmail.setError("required");
+                edtEmail.setError("Email is required");
             }
 
-            if (TextUtils.isEmpty(password)){
+            if (TextUtils.isEmpty(password)) {
                 isEmptyField = true;
-                edtPassword.setError("required");
+                edtPassword.setError("Password is required");
             }
 
-            if (!isEmptyField){
-                mProgressDialog.show();
+            if (!isEmptyField) {
+                progressBar.setVisibility(View.VISIBLE);
                 mFirebaseAuth.signInWithEmailAndPassword(email, password)
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
 
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
-                                if(task.isSuccessful()){
-                                    mProgressDialog.dismiss();
-                                    Toast.makeText(LoginActivity.this, "Login user berhasil",Toast.LENGTH_SHORT).show();
-
+                                if (task.isSuccessful()) {
+                                    Log.d("mytask", task.toString());
                                     AppPreference mAppPreference = new AppPreference(LoginActivity.this);
                                     mAppPreference.setEmail(email);
-
+                                    progressBar.setVisibility(View.INVISIBLE);
                                     startActivity(new Intent(LoginActivity.this, MainActivity.class));
                                     finish();
-                                }else {
-                                    Toast.makeText(LoginActivity.this, "Login user gagal",Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
                                 }
                             }
                         });
--- app/src/main/java/firebase/com/labs/ramdani/mygroupchat/RegisterActivity.java
+++ app/src/main/java/firebase/com/labs/ramdani/mygroupchat/RegisterActivity.java
@@ -1,16 +1,14 @@
 package firebase.com.labs.ramdani.mygroupchat;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -22,100 +20,102 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
     private EditText edtName, edtPassword, edtEmail;
     private Button btnRegister;
-    private TextView tvLogin;
+    private Button btn_login;
+    private ProgressBar progress;
 
     //initial
     private FirebaseAuth mFirebaseAuth;
 
-    private ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        edtName = (EditText) findViewById(R.id.edt_name);
-        edtPassword = (EditText) findViewById(R.id.edt_password);
-        edtEmail = (EditText) findViewById(R.id.edt_email);
-        btnRegister = (Button) findViewById(R.id.btn_register);
-
-        btnRegister.setOnClickListener(this);
-        tvLogin = (TextView) findViewById(R.id.tv_login);
-        tvLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
-                finish();
-            }
-        });
 
         //initial
         mFirebaseAuth = FirebaseAuth.getInstance();
-
-        mProgressDialog = new ProgressDialog(this);
-        mProgressDialog.setTitle("Register");
-        mProgressDialog.setMessage("Please wait....");
+        setupUI();
 
         AppPreference mAppPreference = new AppPreference(this);
-        if (!TextUtils.isEmpty(mAppPreference.getEmail())){
+        if (!TextUtils.isEmpty(mAppPreference.getEmail())) {
             startActivity(new Intent(RegisterActivity.this, MainActivity.class));
             finish();
         }
 
     }
 
+    public void setupUI() {
+        edtName = (EditText) findViewById(R.id.edt_name);
+        edtPassword = (EditText) findViewById(R.id.edt_password);
+        edtEmail = (EditText) findViewById(R.id.edt_email);
+        btnRegister = (Button) findViewById(R.id.btn_register);
+        progress = (ProgressBar) findViewById(R.id.progress);
+        progress.setVisibility(View.INVISIBLE);
+
+        btnRegister.setOnClickListener(this);
+        btn_login = (Button) findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(this);
+    }
 
     @Override
     public void onClick(View v) {
 
-        if (v.getId() == R.id.btn_register){
-            boolean isEmptyField = false;
+        switch (v.getId()) {
+            case R.id.btn_register:
+                boolean isEmptyField = false;
 
-            String name  = edtName.getText().toString().trim();
-            String email  = edtEmail.getText().toString().trim();
-            String password  = edtPassword.getText().toString().trim();
+                String name = edtName.getText().toString().trim();
+                String email = edtEmail.getText().toString().trim();
+                String password = edtPassword.getText().toString().trim();
 
-            //validasi
-            if (TextUtils.isEmpty(name)) {
-                isEmptyField = true;
-                edtName.setError("required");
-            }
+                //validasi
+                if (TextUtils.isEmpty(name)) {
+                    isEmptyField = true;
+                    edtName.setError("Display name is required");
+                }
 
-            if (TextUtils.isEmpty(email)){
-                isEmptyField = true;
-                edtEmail.setError("required");
-            }
+                if (TextUtils.isEmpty(email)) {
+                    isEmptyField = true;
+                    edtEmail.setError("Email is required");
+                }
 
-            if (TextUtils.isEmpty(password)){
-                isEmptyField = true;
-                edtPassword.setError("required");
-            }
+                if (TextUtils.isEmpty(password)) {
+                    isEmptyField = true;
+                    edtPassword.setError("Password is required");
+                } else if (password.length() < 6) {
+                    isEmptyField = true;
+                    edtPassword.setError("Password length should be more than 5 characters");
+                }
 
-            if (!isEmptyField){
-                mProgressDialog.show();
+                if (!isEmptyField) {
+                    progress.setVisibility(View.VISIBLE);
 
-                mFirebaseAuth.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    mFirebaseAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
 
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if(task.isSuccessful()){
-                                    mProgressDialog.dismiss();
-                                    Toast.makeText(RegisterActivity.this, "Register user berhasil",Toast.LENGTH_SHORT).show();
-                                    startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
-                                    finish();
-                                }else {
-                                    Toast.makeText(RegisterActivity.this, "Register user gagal",Toast.LENGTH_SHORT).show();
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(RegisterActivity.this, "Register successfully", Toast.LENGTH_SHORT).show();
+                                        progress.setVisibility(View.INVISIBLE);
+                                        startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                                        finish();
+                                    } else {
+                                        progress.setVisibility(View.INVISIBLE);
+                                        Toast.makeText(RegisterActivity.this, "Please try again", Toast.LENGTH_SHORT).show();
+                                    }
                                 }
-                            }
-                        });
-            }
+                            });
+                }
+                break;
+
+            case R.id.btn_login:
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                finish();
+                break;
+
         }
-
-
-
-
-
 
 
     }
